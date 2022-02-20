8fc3610dd3dc9fb1e215d8c20713d7df8487ff5a
Marsh Rangel <mrangell75@gmail.com>
2022-02-20 02:12:37 -0600

    Development of view and controller of productHome

--- src/main/java/com/parrot/products_orders/controller/HomeController.java
+++ src/main/java/com/parrot/products_orders/controller/HomeController.java
@@ -1,11 +1,16 @@
 package com.parrot.products_orders.controller;
 
+import com.parrot.products_orders.entity.Productos;
 import com.parrot.products_orders.service.ProductosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
 
 @Controller
 @RequestMapping("/")
@@ -18,5 +23,16 @@ public class HomeController {
     public String home(Model modelo) {
         modelo.addAttribute("productos", productosService.findAllProducts());
         return "usuario/home";
+    }
+
+    @GetMapping("productohome/{id}")
+    private String productoHome(@PathVariable Long id, Model modelo) {
+        Productos producto = new Productos();
+        Optional<Productos> productoOptional= productosService.get(id);
+        producto = productoOptional.get();
+
+        modelo.addAttribute("producto", producto);
+
+        return "usuario/productohome";
     }
 }
